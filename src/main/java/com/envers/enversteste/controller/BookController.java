0df19964fa78d4c7619e4cae97f3b958db79b3de
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-10-05 13:32:22 -0300

    Created method post for clas BookController

--- src/main/java/com/envers/enversteste/controller/BookController.java
+++ src/main/java/com/envers/enversteste/controller/BookController.java
@@ -1,4 +1,28 @@
 package com.envers.enversteste.controller;
 
+import com.envers.enversteste.model.Book;
+import com.envers.enversteste.repository.BookRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        if(book.getId() != null) {
+            throw new IllegalArgumentException("The ID will be set by the database!");
+        }
+        return bookRepository.save(book);
+    }
+
 }
