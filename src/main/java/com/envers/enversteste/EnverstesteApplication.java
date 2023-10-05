63872aa7addbad9cd409a49ea1110649b4e20a31
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-10-05 13:07:51 -0300

    Criado BookRevisionRepository e BookRepository

--- src/main/java/com/envers/enversteste/EnverstesteApplication.java
+++ src/main/java/com/envers/enversteste/EnverstesteApplication.java
@@ -2,8 +2,10 @@ package com.envers.enversteste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.config.EnableEnversRepositories;
 
 @SpringBootApplication
+@EnableEnversRepositories
 public class EnverstesteApplication {
 
 	public static void main(String[] args) {
