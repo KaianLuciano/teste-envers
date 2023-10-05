68b5e63f77b6f592577d896c7e39aaac5edcccf1
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-10-05 13:16:07 -0300

    Criado método getCreatorUsername

--- src/main/java/com/envers/enversteste/controller/BookHistoryController.java
+++ src/main/java/com/envers/enversteste/controller/BookHistoryController.java
@@ -1,9 +1,11 @@
 package com.envers.enversteste.controller;
 
+import com.envers.enversteste.envers.AuditEnversInfo;
 import com.envers.enversteste.model.Book;
 import com.envers.enversteste.repository.BookRevisionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.history.Revision;
+import org.springframework.data.history.RevisionMetadata;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +22,19 @@ public class BookHistoryController {
 
     @GetMapping("/revisions/{id}")
     public List<Book> getBookRevisions(@PathVariable Long id) {
-        bookRevisionRepository
+        return bookRevisionRepository
                 .findRevisions(id)
                 .stream()
                 .map(Revision::getEntity)
                 .toList();
     }
+
+    @GetMapping("/creator/{id}")
+    public String getCreatorUsername(@PathVariable Long id) {
+        Revision<Integer, Book> revision = bookRevisionRepository.findRevision(id,1).orElseThrow();
+        RevisionMetadata<Integer> metadata = revision.getMetadata();
+        AuditEnversInfo auditEnversInfo = metadata.getDelegate();
+        return auditEnversInfo.getUsername();
+    }
+    
 }
